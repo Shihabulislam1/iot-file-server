3a4d029d617dce2aa5231552b1aeb7767908ce19
Md Shihabul Islam <mdshihabulislam.mte.ruet@gmail.com>
2023-12-08 18:38:03 +0600

    blockchain add

--- app.js
+++ app.js
@@ -1,72 +1,84 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const fs = require("fs");
-const path = require("path"); // Node.js path module for working with file paths
+const Blockchain = require("./conroller/main");
+const Block = require("./conroller/block"); // Import Block class from controller/block.js
+
 const app = express();
 
-// Middleware to parse JSON body
 app.use(bodyParser.json());
-// get method to deliver simple text to the browser
+// Initialize the blockchain outside of the endpoint handler
+let edgeCoin = new Blockchain();
+
 app.get("/", (req, res) => {
   res.send("Hello From Zishan!");
 });
-// Endpoint to handle incoming sensor data
+
+// Get all block information
+app.get("/blocks", (req, res) => {
+  res.status(200).json({ blocks: edgeCoin.chain });
+});
+
+// Calculate the number of blocks created in one-hour intervals from genesis block creation time
+app.get("/blocks/created-per-hour", (req, res) => {
+  const genesisBlockTimestamp = new Date(edgeCoin.chain[0].timestamp);
+  const currentTime = new Date();
+
+  const hourDifference = Math.floor(
+    (currentTime - genesisBlockTimestamp) / (60 * 60 * 1000)
+  ); // Total hours elapsed
+
+  const blocksPerHour = {};
+
+  for (let i = 0; i <= hourDifference; i++) {
+    const startTimestamp = new Date(
+      genesisBlockTimestamp.getTime() + i * 60 * 60 * 1000
+    );
+    const endTimestamp = new Date(
+      genesisBlockTimestamp.getTime() + (i + 1) * 60 * 60 * 1000
+    );
+
+    const blocksInHour = edgeCoin.chain.filter(
+      (block) =>
+        new Date(block.timestamp) >= startTimestamp &&
+        new Date(block.timestamp) < endTimestamp
+    );
+
+    blocksPerHour[`Hour ${i + 1}`] = blocksInHour.length;
+  }
+
+  res.status(200).json({ blocksPerHour });
+});
+
+// Get the count of connected servers (distinct serverHash values)
+app.get("/servers/count", (req, res) => {
+  const connectedServers = new Set(
+    edgeCoin.chain.map((block) => block.serverHash)
+  );
+  res.status(200).json({ count: connectedServers.size });
+});
+
+// Get block information by hash
+app.get("/blocks/:hash", (req, res) => {
+  const { hash } = req.params;
+  const block = edgeCoin.chain.find((block) => block.hash === hash);
+  if (block) {
+    res.status(200).json({ block });
+  } else {
+    res.status(404).json({ message: "Block not found" });
+  }
+});
+
 app.post("/", (req, res) => {
   const tempData = req.body; // Assuming JSON data contains temperature and humidity
 
-  const dataToSave = { data:`tempData`, timestamp: new Date() };
-  const directoryPath = path.join(__dirname, "public"); // Path to your public folder
+  const dataToSave = { data: tempData, timestamp: new Date() };
 
-  // Ensure the 'public' directory exists; if not, create it
-  if (!fs.existsSync(directoryPath)) {
-    fs.mkdirSync(directoryPath);
-  }
+  const blockIndex = edgeCoin.chain.length; // Determine the index correctly based on the chain length
 
-  // Save data to a new file (you may want to use a better file naming convention)
-  const fileName = `sensor_data_${Date.now()}.json`;
-  const filePath = path.join(directoryPath, fileName);
+  const newBlock = new Block(blockIndex, `${new Date()}`, dataToSave);
+  edgeCoin.addBlock(newBlock);
 
-  fs.writeFile(filePath, JSON.stringify(dataToSave), (err) => {
-    if (err) {
-      console.error("Error saving file:", err);
-      res.status(500).send("Error saving data");
-    } else {
-      console.log("Data saved successfully");
-      res.status(200).send("Data saved");
-    }
-  });
-});
-
-// Endpoint to get information about files in the 'public' folder
-app.get("/files-info", (req, res) => {
-  const directoryPath = path.join(__dirname, "public"); // Path to your public folder
-
-  fs.readdir(directoryPath, (err, files) => {
-    if (err) {
-      console.error("Error reading directory:", err);
-      res.status(500).send("Error reading directory");
-    } else {
-      const filesInfo = [];
-      files.forEach((file) => {
-        const filePath = path.join(directoryPath, file);
-        const stats = fs.statSync(filePath);
-        filesInfo.push({
-          fileName: file,
-          sizeInBytes: stats.size,
-          createdAt: stats.birthtime,
-
-          // You can use stats.mtime for modification time
-        });
-      });
-      res.status(200).json({
-        message: "success",
-        file_number: files.length,
-       
-          filesInfo,
-        
-      });
-    }
-  });
+  res.status(200).json({ message: "Block added successfully", newBlock });
 });
 
 module.exports = app;
